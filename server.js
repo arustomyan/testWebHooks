96d2c741e218710d82912d96e14abbd4bd9f3f93
arustomyan <90838956+arustomyan@users.noreply.github.com>
2024-05-07 01:28:01 +0300

    commit

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const nodemailer = require("nodemailer");
 const app = express();
 const PORT = 3000;
-const SECRET_TOKEN = "your_secret_token";
+const SECRET_TOKEN = "glpat-ruZ-sjP7nzsskfiqq7Up";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "v.arustomyan1996@gmail.com",
+    pass: "xsga wgql bhrh pcqc",
+  },
+});
+
+const mailOptions = (currentBranch, lastCommit) => ({
+  from: "v.arustomyan1996@gmail.com",
+  to: "task+3607@placebo25.planfix.ru",
+  subject: `Ветка: ${currentBranch}`,
+  text: `Коммит: 
+  ${lastCommit}`,
+});
 
 // Логирование подключения
 app.use((req, res, next) => {
@@ -15,24 +31,34 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 // Обработчик для POST-запросов на /gitlab-webhook
-app.post("/gitlab-webhook", (req, res) => {
+app.post("/gitlab-push-commit", (req, res) => {
   // Валидация подписи (если установлен секретный токен)
-  // const signature = req.headers["x-gitlab-token"];
-  // if (SECRET_TOKEN && signature !== SECRET_TOKEN) {
-  //   console.error("Invalid token");
-  //   return res.status(401).send("Invalid token");
-  // }
+  const signature = req.headers["x-gitlab-token"];
+  if (SECRET_TOKEN && signature !== SECRET_TOKEN) {
+    console.error("Invalid token");
+    return res.status(401).send("Invalid token");
+  }
 
   // Парсинг JSON из тела запроса
   const payload = req.body;
 
-  // if (!payload) {
-  //   console.error("Empty payload");
-  //   return res.status(400).send("Empty payload");
-  // }
+  if (!payload) {
+    console.error("Empty payload");
+    return res.status(400).send("Empty payload");
+  }
 
   // Обработка полученных данных
   console.log("Received webhook payload:", payload);
+
+  payload.commits.forEach((element) => {
+    transporter.sendMail(mailOptions(payload.ref, element.message), function (error, info) {
+      if (error) {
+        console.error("Error:", error);
+      } else {
+        console.log("Email sent:", info.response);
+      }
+    });
+  });
 
   // Отправка ответа
   res.status(200).send("Webhook received successfully");
